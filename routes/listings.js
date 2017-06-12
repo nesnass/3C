e9a40939ec1b6f9174a5434a3ad509dd5b47e9e4
richardengagelab <richardn@uio.im>
2017-06-12 09:30:38 +0200

    Enable all sources in contribution search

--- routes/listings.js
+++ routes/listings.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Contribution.find({ "origin": "instagram" })
+  Contribution.find({})
     .sort({"created": 'desc'})
     .lean()
     .exec(function (error, items) {
